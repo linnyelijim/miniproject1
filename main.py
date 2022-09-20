516970774c33445eb716ab894f30c027aa9fa12f
Lindsey Jimenez <lindseyjimenez@gmail.com>
2022-09-19 22:21:50 -0400

    Created separate figures for each ticker chart. Added titles and bullet point ticker marks.

--- main.py
+++ main.py
@@ -6,53 +6,82 @@ import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Empty arrays for each ticker
 msftPrices = []
 tlryPrices = []
 gmePrices = []
 nvdaPrices = []
 abnbPrices = []
 
+# Data download for MSFT
 msftdata = yf.download("MSFT", start="2022-09-02", end="2022-09-18")
 for prices in msftdata['Adj Close']:
     msftPrices.append(prices)
 
+# Data download for TLRY
 tlrydata = yf.download("TLRY", start="2022-09-02", end="2022-09-18")
 for prices in tlrydata['Adj Close']:
     tlryPrices.append(prices)
 
+# Data download for GME
 gmedata = yf.download("GME", start="2022-09-02", end="2022-09-18")
 for prices in gmedata['Adj Close']:
     gmePrices.append(prices)
 
+# Data download for NVDA
 nvdadata = yf.download("NVDA", start="2022-09-02", end="2022-09-18")
 for prices in nvdadata['Adj Close']:
     nvdaPrices.append(prices)
 
+# Data download for ABNB
 abnbdata = yf.download("ABNB", start="2022-09-02", end="2022-09-18")
 for prices in abnbdata['Adj Close']:
     abnbPrices.append(prices)
 
+# Prints each price array
 print(msftPrices)
 print(tlryPrices)
 print(gmePrices)
 print(nvdaPrices)
 print(abnbPrices)
 
-# Create a NumPy array
+# Create a NumPy arrays
 msftarray = np.array(msftPrices)
 tlryarray = np.array(tlryPrices)
 gmearray = np.array(gmePrices)
 nvdaarray = np.array(nvdaPrices)
 abnbarray = np.array(abnbPrices)
 
-# Create matplotlib graph
-plt.plot(msftarray)
-plt.plot(tlryarray)
-plt.plot(gmearray)
-plt.plot(nvdaarray)
-plt.plot(abnbPrices)
 
-# Show the graph
-plt.show()
+# Create matplotlib graph MSFT
+fig1 = plt.figure("MSFT")
+plt.title("Microsoft : MSFT")
+plt.plot(msftarray, 'bo-')
+
+
+# Create matplotlib graph TLRY and show
+fig2 = plt.figure("TLRY")
+plt.title("Tilray : TLRY")
+plt.plot(tlryarray, 'bo-')
+
+
+# Create matplotlib graph GME and show
+fig3 = plt.figure("GME")
+plt.title("GameStop : GME")
+plt.plot(gmearray, 'bo-')
+
+
+# Create matplotlib graph NVDA and show
+fig4 = plt.figure("NVDA")
+plt.title("Nvidia : NVDA")
+plt.plot(nvdaarray, 'bo-')
+
+
+# Create matplotlib graph ABNB and show
+fig5 = plt.figure("ABNB")
+plt.title("Airbnb : ABNB")
+plt.plot(abnbPrices, 'bo-')
+
+
 
 
